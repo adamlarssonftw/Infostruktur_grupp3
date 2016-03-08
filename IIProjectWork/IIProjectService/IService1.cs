e22d4ad5cc9808591177272c4ab15db8896cf9a3
erikbrink <erik.o.brink@gmail.com>
2016-03-08 15:01:52 +0100

    ny ändring

--- IIProjectWork/IIProjectService/IService1.cs
+++ IIProjectWork/IIProjectService/IService1.cs
@@ -8,7 +8,7 @@ using System.Text;
 
 namespace IIProjectService
 {
-    //test
+    //har du senaste nu?
 
     // NOTE: You can use the "Rename" command on the "Refactor" menu to change the interface name "IService1" in both code and config file together.
     [ServiceContract]
