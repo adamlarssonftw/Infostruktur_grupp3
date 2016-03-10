3b6ee9315d6a7c1aa9de8773e84406162c4f6f8f
erikbrink <erik.o.brink@gmail.com>
2016-03-10 11:32:57 +0100

    Nytt

---
+++ IIProjectWork/IIProjectClient/Controllers/ProjectController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+//using IIProjectClient.Models;
+
+
+
+namespace IIProjectClient.Controllers
+{
+    public class ProjectController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
